2e3eb0d4ac4f6b76808b0867be433812b2eac0d6
Anastasia Boyko <boyko_anastasia@mail.ru>
2022-11-18 20:51:28 +0300

    added TextBoxTests

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,64 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+
+
+    @BeforeAll
+    static void beforeALL() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+    @Test
+    void fillFormTest () {
+        open ("/automation-practice-form");
+
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue("Anastasiya");
+        $("#lastName").setValue("Boyko");
+        $("#userEmail").setValue("boyko_anastasia@mail.ru");
+        $(byText("Female")).click();
+        $("#userNumber").setValue("88005000552");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("August");
+        $(".react-datepicker__year-select").selectOption("1991");
+        $(".react-datepicker__day--007").click();
+        $("#subjectsInput").setValue("Biology").pressEnter();
+        $(byText("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/Asha.jpg"));
+        $("#currentAddress").setValue("Bolshaya Pirogovskaya 36,53");
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Anastasiya Boyko"));
+        $(".table-responsive").shouldHave(text("boyko_anastasia@mail.ru"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("8800500055"));
+        $(".table-responsive").shouldHave(text("07 August,1991"));
+        $(".table-responsive").shouldHave(text("Biology"));
+        $(".table-responsive").shouldHave(text("Reading"));
+        $(".table-responsive").shouldHave(text("Asha.jpg"));
+        $(".table-responsive").shouldHave(text("Bolshaya Pirogovskaya 36,53"));
+        $(".table-responsive").shouldHave(text("Haryana Karna"));
+
+
+
+
+
+
+    }
+}
